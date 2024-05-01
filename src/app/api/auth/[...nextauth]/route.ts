1f0f2d578834de568d8f7595c82d0ac09859d8a8
waseemzahid <34806507+waseemzahid@users.noreply.github.com>
2024-05-01 21:47:19 +0500

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -12,10 +12,10 @@ const authOptions = {
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_CLIENT_ID!,
-    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    // }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
     // LinkedInProvider({
     //   clientId: process.env.LINKEDIN_CLIENT_ID!,
     //   clientSecret: process.env.LINKEDIN_CLIENT_SECRET!,
